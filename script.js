3a667f9f2d60fb5a5b0a98beddf034a1f8f10159
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-01 10:19:11 +0200

    Add play round logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
     let computerChoice;
     let randomNum = Math.floor(Math.random()*3);
@@ -11,9 +13,43 @@ function getComputerChoice(){
         computerChoice = 'scissors';
         console.log('Computer choice is: scissors');
     }
+    return computerChoice;
 }
 function getHumanChoice(){
     let humanChoice = prompt("Choose between rock, paper or scissors: ");
     humanChoice = humanChoice.toLowerCase();
     console.log("Your choice is: " + humanChoice)
+    return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === 'paper' && computerChoice === 'rock'){
+        humanScore += 1;
+        console.log('Congrats! You won. You now have ' + humanScore + " victories");
+    } else if (humanChoice === 'paper' && computerChoice === 'paper'){
+        console.log("It's a draw.");
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+        computerScore += 1;
+        console.log('You lost! Computer now has ' + computerScore + ' victories');
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        humanScore += 1;
+        console.log('Congrats! You won. You now have ' + humanScore + " victories");
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        computerScore += 1;
+        console.log('You lost! Computer now has ' + computerScore + ' victories');
+    } else if (humanChoice === 'scissors' && computerChoice === 'scissors'){
+        console.log("It's a draw.");
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        humanScore += 1;
+        console.log('Congrats! You won. You now have ' + humanScore + " victories");
+    } else if (humanChoice === 'rock' && computerChoice === 'paper'){
+        computerScore += 1;
+        console.log('You lost! Computer now has ' + computerScore + ' victories');
+    } else if (humanChoice === 'rock' && computerChoice === 'rock'){
+        console.log("It's a draw.");
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
